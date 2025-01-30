3e402a55edabd12e42991182fb24824dbd7c362b
prinaa20 <prinabhagat.it22@scet.ac.in>
2025-01-30 10:02:13 +0530

    Update App.js

--- app5/src/App.js
+++ app5/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import NotesApp from './NotesApp';
+import SignUpForm from './components/SignUpForm'; // Make sure this path is correct
+import NotesApp from './components/NotesApp'; // Make sure this path is correct
 
 function App() {
   return (
     <div className="App">
-      <NotesApp />
+      <h1>Hello World!</h1>
+
+      {/* Sign-Up Form */}
+      <div>
+        <h2>Sign Up Form</h2>
+        <SignUpForm />
+      </div>
+
+      {/* Notes Application */}
+      <div>
+        <h2>Notes Application</h2>
+        <NotesApp />
+      </div>
     </div>
   );
 }
